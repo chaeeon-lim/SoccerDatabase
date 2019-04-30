b72fb85657cafe4d567591063caf3636407ae181
JYB17 <37678894+JYB17@users.noreply.github.com>
2019-04-30 19:40:58 +0900

    added AppearDAO.java

---
+++ java/src/com/soccerdb/oldschool/db/dao/AppearDAO.java
@@ -0,0 +1,13 @@
+package com.soccerdb.oldschool.db.dao;
+
+
+
+import java.util.List;
+
+import com.soccerdb.oldschool.db.entity.Appear;
+import com.soccerdb.oldschool.db.source.GenericDAO;
+
+public interface AppearDAO extends GenericDAO<Appear, Integer>{
+    //TODO please give dditional methods for 'Appear' entity.
+    
+}
