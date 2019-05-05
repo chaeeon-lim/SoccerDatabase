37c78eba2c1866d4d400b29095e412458166814b
geun12 <44759115+geun12@users.noreply.github.com>
2019-05-05 14:26:28 +0900

    game,matchDAO&ImplDAO

---
+++ java/src/com/soccerdb/oldschool/db/dao/GameDAO.java
@@ -0,0 +1,9 @@
+package com.soccerdb.oldschool.db.dao;
+import java.util.List;
+
+import com.soccerdb.oldschool.db.entity.Game;
+import com.soccerdb.oldschool.db.source.GenericDAO;
+
+public interface GameDAO extends GenericDAO<Game, Integer>{
+    //TODO please give dditional methods for 'Game' entity.
+}
