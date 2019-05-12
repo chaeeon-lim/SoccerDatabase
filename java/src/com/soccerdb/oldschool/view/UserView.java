b2af443bce1dcaf590e57bf4cf213199c10c9714
JYB17 <37678894+JYB17@users.noreply.github.com>
2019-05-13 02:59:25 +0900

    AdminView, UserView updated - buttons added

--- java/src/com/soccerdb/oldschool/view/UserView.java
+++ java/src/com/soccerdb/oldschool/view/UserView.java
@@ -8,24 +8,39 @@ import java.awt.event.*;
 import java.util.List;
 
 import com.soccerdb.oldschool.db.dao.*;
-import com.soccerdb.oldschool.db.dao.*;
 import com.soccerdb.oldschool.db.entity.*;
 
 public class UserView {
 	JTextArea text = new JTextArea("", 20, 75);
 	JScrollPane sp = new JScrollPane(text);
-	JButton seasonsButton = new JButton("Seasons");
-	JButton leaguesButton = new JButton("Leagues");
-	JButton clubsButton = new JButton("Clubs");
-	JButton playersButton = new JButton("Players");
-	JTextArea searchText = new JTextArea("", 1, 40);
+	JButton seasonsButton = new JButton("Season");
+	JButton leaguesButton = new JButton("League");
+	JButton clubsButton = new JButton("Club");
+	JButton playersButton = new JButton("Player");
+	JButton appearButton = new JButton("Appear");
+	JButton btlButton = new JButton("Belong_to_League");
+	JButton contractButton = new JButton("Contract");
+	JButton gameButton = new JButton("Game");
+	JButton matchButton = new JButton("Match");
+	JButton ppsButton = new JButton("Player_per_Season");
+	JTextArea searchText = new JTextArea("", 1, 70);
 	JButton searchButton = new JButton("Search");
 	JButton topTenButton = new JButton("Top10");
+	JButton cancelButton = new JButton("Cancel All");
+
+	JButton[] entityButtons = {appearButton, btlButton, clubsButton, contractButton, gameButton, 
+									leaguesButton, matchButton, playersButton, seasonsButton, ppsButton};
 
 	SeasonDAO seasonDAO = new ImplSeasonDAO();
 	LeagueDAO leagueDAO = new ImplLeagueDAO();
 	ClubDAO clubDAO = new ImplClubDAO();
 	PlayerDAO playerDAO = new ImplPlayerDAO();
+	AppearDAO apearDAO = new ImplAppearDAO();
+	BTLDAO btlDAO = new ImplBTLDAO();
+	ContractDAO contractDAO = new ImplContractDAO();
+	GameDAO gameDAO = new ImplGameDAO();
+	MatchDAO matchDAO = new ImplMatchDAO();
+	PPSDAO ppsDAO = new ImplPPSDAO();
 
 	public void showUser(JFrame frame, JPanel panel){
 		frame.setTitle("User Mode");
@@ -45,6 +60,10 @@ public class UserView {
 					} catch(Exception ex){
 						ex.printStackTrace();
 					}
+					for(JButton buttons : entityButtons){
+						buttons.setEnabled(false);
+					}
+					seasonsButton.setEnabled(true);
 				}
 			}
 		);
@@ -63,6 +82,10 @@ public class UserView {
 					} catch(Exception ex){
 						ex.printStackTrace();
 					}
+					for(JButton buttons : entityButtons){
+						buttons.setEnabled(false);
+					}
+					leaguesButton.setEnabled(true);
 				}
 			}
 		);
@@ -81,6 +104,10 @@ public class UserView {
 					} catch(Exception ex){
 						ex.printStackTrace();
 					}
+					for(JButton buttons : entityButtons){
+						buttons.setEnabled(false);
+					}
+					clubsButton.setEnabled(true);
 				}
 			}
 		);
@@ -99,17 +126,150 @@ public class UserView {
 					} catch(Exception ex){
 						ex.printStackTrace();
 					}
+					for(JButton buttons : entityButtons){
+						buttons.setEnabled(false);
+					}
+					playersButton.setEnabled(true);
 				}
 			}
 		);
 
-		panel.add(seasonsButton);
-		panel.add(leaguesButton);
-		panel.add(clubsButton);
-		panel.add(playersButton);
+		appearButton.addActionListener(
+			new ActionListener(){
+				public void actionPerformed(ActionEvent ae){
+					try{
+
+					} catch(Exception ex){
+						ex.printStackTrace();
+					}
+					for(JButton buttons : entityButtons){
+						buttons.setEnabled(false);
+					}
+					appearButton.setEnabled(true);
+
+				}
+			}
+		);
+
+		btlButton.addActionListener(
+			new ActionListener(){
+				public void actionPerformed(ActionEvent ae){
+					try{
+
+					} catch(Exception ex){
+						ex.printStackTrace();
+					}
+					for(JButton buttons : entityButtons){
+						buttons.setEnabled(false);
+					}
+					btlButton.setEnabled(true);
+
+				}
+			}
+		);
+
+		contractButton.addActionListener(
+			new ActionListener(){
+				public void actionPerformed(ActionEvent ae){
+					try{
+
+					} catch(Exception ex){
+						ex.printStackTrace();
+					}
+					for(JButton buttons : entityButtons){
+						buttons.setEnabled(false);
+					}
+					contractButton.setEnabled(true);
+
+				}
+			}
+		);
+
+		gameButton.addActionListener(
+			new ActionListener(){
+				public void actionPerformed(ActionEvent ae){
+					try{
+
+					} catch(Exception ex){
+						ex.printStackTrace();
+					}
+					for(JButton buttons : entityButtons){
+						buttons.setEnabled(false);
+					}
+					gameButton.setEnabled(true);
+
+				}
+			}
+		);
+
+		matchButton.addActionListener(
+			new ActionListener(){
+				public void actionPerformed(ActionEvent ae){
+					try{
+
+					} catch(Exception ex){
+						ex.printStackTrace();
+					}
+					for(JButton buttons : entityButtons){
+						buttons.setEnabled(false);
+					}
+					matchButton.setEnabled(true);
+
+				}
+			}
+		);
+
+		ppsButton.addActionListener(
+			new ActionListener(){
+				public void actionPerformed(ActionEvent ae){
+					try{
+
+					} catch(Exception ex){
+						ex.printStackTrace();
+					}
+					for(JButton buttons : entityButtons){
+						buttons.setEnabled(false);
+					}
+					ppsButton.setEnabled(true);
+
+				}
+			}
+		);
+
+		cancelButton.addActionListener(
+			new ActionListener(){
+				public void actionPerformed(ActionEvent ae){
+					for(JButton buttons : entityButtons){
+						buttons.setEnabled(true);
+					}
+					text.setText("");
+
+				}
+			}
+		);
+
+		searchButton.addActionListener(
+			new ActionListener(){
+				public void actionPerformed(ActionEvent ae){
+					try{
+
+					} catch(Exception ex){
+						ex.printStackTrace();
+					}
+					for(JButton buttons : entityButtons){
+						buttons.setEnabled(true);
+					}
+				}
+			}
+		);
+
+
+		for(JButton buttons : entityButtons){
+			panel.add(buttons);
+		}
+		panel.add(cancelButton);
 		panel.add(searchText);
 		panel.add(searchButton);
-		panel.add(topTenButton);
 		panel.add(sp);
 	}
 
