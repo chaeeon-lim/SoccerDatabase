e58437a8f7aae8ab69388d46154b0a7dceda4a9c
geun12 <44759115+geun12@users.noreply.github.com>
2019-05-18 16:13:40 +0900

    Match,Game DAO

--- java/src/com/soccerdb/oldschool/db/dao/ImplMatchDAO.java
+++ java/src/com/soccerdb/oldschool/db/dao/ImplMatchDAO.java
@@ -11,9 +11,9 @@ public class ImplMatchDAO extends ImplAbstractDAO<Match, Integer> implements Mat
 		super();
 		namespace = "com.soccerdb.oldschool.db.dao.MatchDAO";
 	}
-
 	@Override
 	public List<Match> selectByGameId(int game_id) throws Exception {
+		session = getSqlSessionFactory().openSession();
 		try {
 			session = getSqlSessionFactory().openSession();
 			return session.selectList(namespace + ".selectById", game_id);
@@ -23,6 +23,7 @@ public class ImplMatchDAO extends ImplAbstractDAO<Match, Integer> implements Mat
 	}
 	@Override
 	public List<Match> selectByClubId(int club_id) throws Exception {
+		session = getSqlSessionFactory().openSession();
 		try {
 			session = getSqlSessionFactory().openSession();
 			return session.selectList(namespace + ".selectById",club_id);
@@ -32,6 +33,7 @@ public class ImplMatchDAO extends ImplAbstractDAO<Match, Integer> implements Mat
 	}
 	@Override
 	public List<Match> selectBySeasonId(int season_id) throws Exception {
+		session = getSqlSessionFactory().openSession();
 		try {
 			session = getSqlSessionFactory().openSession();
 			return session.selectList(namespace + ".selectById",season_id);
@@ -40,7 +42,49 @@ public class ImplMatchDAO extends ImplAbstractDAO<Match, Integer> implements Mat
 		}
 	}
 	@Override
+	public List<Match> selectByIsHome(boolean is_home) throws Exception {
+		session = getSqlSessionFactory().openSession();
+		try {
+			session = getSqlSessionFactory().openSession();
+			return session.selectList(namespace + ".selectByIsHome",is_home);
+		}finally {
+			session.close();
+		}
+	}
+
+	@Override
+	public List<Match> selectORderByGameId() throws Exception {
+		session = getSqlSessionFactory().openSession();
+		try {
+			session = getSqlSessionFactory().openSession();
+			return session.selectList(namespace + ".selectOrderByGameId");
+		}finally {
+			session.close();
+		}
+	}
+	@Override
+	public List<Match> selectORderByClubId() throws Exception {
+		session = getSqlSessionFactory().openSession();
+		try {
+			session = getSqlSessionFactory().openSession();
+			return session.selectList(namespace + ".selectOrderByClubId");
+		}finally {
+			session.close();
+		}
+	}
+	@Override
+	public List<Match> selectORderBySeasonId() throws Exception {
+		session = getSqlSessionFactory().openSession();
+		try {
+			session = getSqlSessionFactory().openSession();
+			return session.selectList(namespace + ".selectOrderBySeasonId");
+		}finally {
+			session.close();
+		}
+	}
+	@Override
 	public void deleteMatch(Match match) throws Exception {
+		session = getSqlSessionFactory().openSession();
 		try {
 			session = getSqlSessionFactory().openSession();
 			session.delete(namespace + ".deleteMatch", match);
@@ -48,4 +92,15 @@ public class ImplMatchDAO extends ImplAbstractDAO<Match, Integer> implements Mat
 			session.close();
 		}
 	}
+	@Override
+	public void updateMatch(Match match) throws Exception {
+		session = getSqlSessionFactory().openSession();
+		try {
+			session = getSqlSessionFactory().openSession();
+			session.update(namespace + ".updateMatch", match);
+		}finally {
+			session.close();
+		}
+	}
+	
 }
--- java/src/com/soccerdb/oldschool/db/dao/MatchDAO.java
+++ java/src/com/soccerdb/oldschool/db/dao/MatchDAO.java
@@ -6,8 +6,15 @@ import com.soccerdb.oldschool.db.source.GenericDAO;
 
 public interface MatchDAO extends GenericDAO<Match, Integer>{
     //TODO please give aditional methods for 'Match' entity.
+	public List<Match> selectAll() throws Exception;
 	public List<Match> selectByGameId(int game_id) throws Exception; 
 	public List<Match> selectByClubId(int club_id) throws Exception; 
-	public List<Match> selectBySeasonId(int season_id) throws Exception; 
-  public void deleteMatch(Match match) throws Exception;
+	public List<Match> selectBySeasonId(int season_id) throws Exception;
+	public List<Match> selectByIsHome(boolean is_home) throws Exception;
+	public List<Match> selectORderByGameId() throws Exception;
+	public List<Match> selectORderByClubId() throws Exception;
+	public List<Match> selectORderBySeasonId() throws Exception;
+	
+	public void updateMatch(Match match) throws Exception;
+	public void deleteMatch(Match match) throws Exception;
 }
